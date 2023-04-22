cf5bc703f195c1716d3ed6130152785202c27fb8
Samuel <samuel.b.rodrigues@accenture.com>
2023-04-22 09:48:28 -0300

    rxjs operator to transform data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -33,9 +34,19 @@ export class AppComponent implements OnInit {
     // Send Http request
   }
 
-  private fetchPosts(){
-    this.http.get('https://angular-request-project-default-rtdb.firebaseio.com/posts.json').subscribe(posts=>{
-      console.log(posts);
-    })
+  private fetchPosts() {
+    this.http.get('https://angular-request-project-default-rtdb.firebaseio.com/posts.json')
+      .pipe(map(responseData => {
+        const postArray = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postArray.push({...responseData[key], id: key});
+          }
+        }
+        return postArray;
+      }))
+      .subscribe(posts => {
+        console.log(posts);
+      })
   }
 }
