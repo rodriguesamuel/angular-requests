a15319f4154cfdcb160985dc80dad367f9228d81
Samuel <samuel.b.rodrigues@accenture.com>
2023-04-22 15:44:00 -0300

    using subjects for error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,33 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {map} from "rxjs/operators";
 import {Post} from "./post.model";
 import {PostService} from "./post.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
   isFetchin: boolean = false;
-  error: null;
+  error = null;
+  private errorSub: Subscription;
 
   constructor(private http: HttpClient,
               private postService: PostService) {
   }
 
   ngOnInit() {
+    this.errorSub = this.postService.error.subscribe(errorMessage=>{
+      this.error = errorMessage;
+    });
     this.getPosts();
+  }
+
+  ngOnDestroy() {
+    this.errorSub.unsubscribe();
   }
 
   onCreatePost(postData: Post) {
--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -2,9 +2,11 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Post} from "./post.model";
 import {map} from "rxjs/operators";
+import {Subject} from "rxjs";
 
 @Injectable({providedIn: 'root'})
 export class PostService {
+  error = new Subject<string>();
   constructor(private http: HttpClient) {
   }
 
@@ -15,7 +17,7 @@ export class PostService {
       .subscribe(responseData => {
         console.log(responseData);
       }, error => {
-
+        this.error.next(error.message);
       });
   }
 
