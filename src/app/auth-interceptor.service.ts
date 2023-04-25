ad21f8cfa3aaefd392c5951d086d7794bf836af7
Samuel <samuel.b.rodrigues@accenture.com>
2023-04-24 22:53:55 -0300

    response interceptors

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,5 +1,6 @@
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {tap} from "rxjs/operators";
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -7,7 +8,13 @@ export class AuthInterceptorService implements HttpInterceptor {
     console.log(req.url);
     const newRequest = req.clone(
       {headers: req.headers.append('Auth', 'asdf')});
-    return next.handle(newRequest);
+    return next.handle(newRequest).pipe(tap(event=> {
+      console.log(event);
+      if(event.type === HttpEventType.Response) {
+        console.log('response arrived');
+        console.log(event.body);
+      }
+    }));
   }
 
 }
