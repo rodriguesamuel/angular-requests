95fd2429af29fa306a5cf56d538145aa112bc181
Samuel <samuel.b.rodrigues@accenture.com>
2023-04-24 22:35:25 -0300

    changing the response body type

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {HttpClient, HttpEventType, HttpHeaders, HttpParams} from "@angular/common/http";
 import {Post} from "./post.model";
-import {catchError, map} from "rxjs/operators";
+import {catchError, map, tap} from "rxjs/operators";
 import {Subject, throwError} from "rxjs";
 
 @Injectable({providedIn: 'root'})
@@ -14,7 +14,10 @@ export class PostService {
   createAndStorePost(title: string, content: string) {
     const postData: Post = {title: title, content: content};
     this.http.post<{ name: string }>('https://angular-request-project-default-rtdb.firebaseio.com/posts.json',
-      postData)
+      postData,
+      {
+        observe: 'response'
+      })
       .subscribe(responseData => {
         console.log(responseData);
       }, error => {
@@ -49,6 +52,18 @@ export class PostService {
   }
 
   deletePosts() {
-    return this.http.delete('https://angular-request-project-default-rtdb.firebaseio.com/posts.json');
+    return this.http.delete('https://angular-request-project-default-rtdb.firebaseio.com/posts.json',
+      {
+        observe: 'events',
+        responseType: 'text'
+      }).pipe(tap(event => {
+        console.log(event);
+      if(event.type === HttpEventType.Sent){
+        console.log(event.type);
+      }
+        if(event.type === HttpEventType.Response){
+          console.log(event.body);
+        }
+    }));
   }
 }
